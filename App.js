7c64ab872c4f92d9c9e82d89eed4a5e8103ce717
darmstrong <armstrong2579@gmail.com>
2022-08-23 17:10:17 -0700

    add basic structure to app

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+// change this to wherever the portfolio is stored
+import PortfolioContainer from "./components/PortfolioContainer";
+// change here as well to match
+const App = () => <PortfolioContainer />;
+
+export default App;
