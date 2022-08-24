49dff5d412fc7657a742f6c2bd13da3e116f33cf
darmstrong <armstrong2579@gmail.com>
2022-08-23 17:38:24 -0700

    add contact form

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
+
   return (
     <div>
-      <h1>Contact</h1>
-      <p>
-        content here
-        </p>
+      <form className="form">
+      <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="name"
+          placeholder="name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
